55a82789af9e8f36a9c11e654aad47108315fc3c
Meemom <jemima.silaen@gmail.com>
2026-01-18 02:36:38 -0500

    connected to like events page

--- frontend/app/profile/page.tsx
+++ frontend/app/profile/page.tsx
@@ -164,13 +164,11 @@ export default function ProfilePage() {
           </div>
 
           <div className="profile-actions">
-            <button className="btn-edit" onClick={() => setEditing(!editing)}>
-              <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
-                <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/>
-                <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/>
-              </svg>
-              Edit Profile
-            </button>
+            <Link href="/my_events" style={{ textDecoration: 'none' }}>
+              <button className="btn-edit" onClick={() => setEditing(!editing)}>
+                View Liked Events
+              </button>
+            </Link>
           </div>
         </div>
 
